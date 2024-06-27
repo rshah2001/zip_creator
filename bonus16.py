09cf76bc1adeb124fc67768b5af6b0ae338fcc86
Rishil Shah <90150116+rshah2001@users.noreply.github.com>
2024-06-26 23:19:36 -0400

    Update bonus16.py

--- bonus16.py
+++ bonus16.py
@@ -15,16 +15,17 @@ input2 = sg.Input()
 button2 = sg.FolderBrowse("Choose", key='folder')
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text(key="output")
 
 window = sg.Window("file compressor", layout=[[label1, input1, button1],
                                               [label2, input2, button2],
-                                              [compress_button]])
+                                              [compress_button, output_label]])
 while True:
     event, values = window.read()
     print(event, values)
     filepaths = values['files'].split(";")
     folder = values['folder']
     make_archive(filepaths, folder)
-
+    window["output"].update(values="Compression Completed!!")
 
 window.close()
